81c7c1f450d977e000c4c005fc7f65647dfce772
Sameer Puri <sameer@users.noreply.github.com>
2019-01-06 10:23:59 -0500

    Add standard LfsError code outputs & use Self in impl consts

--- src/batch.rs
+++ src/batch.rs
@@ -72,15 +72,15 @@ pub struct ObjectError {
 }
 
 impl ObjectError {
-    const DOES_NOT_EXIST: ObjectError = ObjectError {
+    const DOES_NOT_EXIST: Self = Self {
         code: Status::NotFound.code,
         message: "Object does not exist",
     };
-    const REMOVED_BY_OWNER: ObjectError = ObjectError {
+    const REMOVED_BY_OWNER: Self = Self {
         code: Status::Gone.code,
         message: "Object removed by owner",
     };
-    const VALIDATION_ERROR: ObjectError = ObjectError {
+    const VALIDATION_ERROR: Self = Self {
         code: Status::UnprocessableEntity.code,
         message: "Validation error",
     };
@@ -115,10 +115,48 @@ pub struct Verify {}
 
 #[derive(PartialEq, Eq, Debug, Serialize)]
 pub struct LfsErrorResponse {
-    message: String,
+    message: &'static str,
     #[serde(with = "url_serde")]
     documentation_url: Option<Url>,
     request_id: Option<String>,
+    #[serde(skip)]
+    status: Status,
+}
+
+impl LfsErrorResponse {
+    const ACCEPT_HEADER_INCORRECT: Self = Self {
+        message: "The Accept header needs to be `application/vnd.git-lfs+json`.",
+        documentation_url: None,
+        request_id: None,
+        status: Status::NotAcceptable,
+    };
+    const RATE_LIMIT_HIT: Self = Self {
+        message: "A rate limit has been hit with the server.",
+        documentation_url: None,
+        request_id: None,
+        status: Status::TooManyRequests,
+    };
+    const NOT_IMPLEMENTED: Self = Self {
+        message: "The server has not implemented the current method.",
+        documentation_url: None,
+        request_id: None,
+        status: Status::NotImplemented,
+    };
+    const INSUFFICIENT_STORAGE: Self = Self {
+        message: "The server has insufficient storage capacity to complete the request.",
+        documentation_url: None,
+        request_id: None,
+        status: Status::InsufficientStorage,
+    };
+    const BANDWIDTH_LIMIT_EXCEEDED: Self = Self {
+        message: "A bandwidth limit has been exceeded.",
+        documentation_url: None,
+        request_id: None,
+        status: Status {
+            code: 509,
+            reason: "Bandwith Limit Exceeded",
+        },
+    };
 }
 
 #[get("/<user>/<repo>/info/lfs/objects/batch")]
@@ -185,9 +223,10 @@ mod test {
         assert_eq!(
             include_str!("test/lfs_error.json"),
             serde_json::to_string_pretty(&LfsErrorResponse {
-                message: "Not found".to_string(),
+                message: "Not found",
                 documentation_url: Some(Url::parse("https://lfs-server.com/docs/errors").unwrap()),
-                request_id: Some("123".to_string())
+                request_id: Some("123".to_string()),
+                status: Status::NotFound
             })
             .unwrap(),
         );
